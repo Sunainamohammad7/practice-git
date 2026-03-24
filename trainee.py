01c0a67db36883758cedc530a73d386e3419c2e8
Sunaina <mohammadsunaina9@gmail.com>
2026-03-24 17:39:44 +0530

    Main change

--- trainee.py
+++ trainee.py
@@ -1,16 +1,2 @@
-def add(a,b):
-    sum=a+b
-    return f"Initial Commit in main {sum}"
-def sub(a,b):
-    sub=a-b
-    return f"Commit made in new branch {sub}"
-def mul(a,b):
-    mul=a*b
-    return f"COmmit made in main branch {mul}"
-
-a=add(2,3) 
-b=sub(5,3)
-c=mul(2,3)
-print(a)
-print(b) 
-print(c)  
+def greet():
+    print("Hello from main")
